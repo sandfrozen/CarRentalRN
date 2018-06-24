a97aa34eb69297ab09d41a785db2a84367c168f2
Tomek Buslowski <tbuslowski@gmail.com>
2018-06-24 14:45:24 +0200

    [Student's project passed on 5/A]

--- Screens/API.js
+++ Screens/API.js
@@ -1,5 +1,8 @@
 const API = {
-  URL: 'http://192.168.1.115:8080/CarRentalREST/v1'
+  // URL: 'http://192.168.1.115:8080/CarRentalREST/v1'
+  // URL: 'http://172.20.10.10:8080/CarRentalREST/v1'
+  URL: 'http://10.211.55.3:8080/CarRentalREST/v1'
+
 }
 
 export default API
